bf02c877daff1c5c81134d4398e2adc2033647e4
Saryn <115831208+Sarynas@users.noreply.github.com>
2025-03-31 23:42:11 -0400

    SAIN 3.11
    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using BepInEx.Configuration;
+
+namespace SAIN
+{
+    [BepInPlugin("com.saryn.sain", "SAIN", "3.11.2")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<Boolean> Enable
+        {
+            get; set;
+        }
+        public static ManualLogSource LogSource;
+
+        private void Awake()
+        {
+            LogSource = Logger;
+            LogSource.LogInfo("APRIL FOOLS! HAHAHA!");
+            {
+
+                Enable = Config.Bind("Settings", "Enable", false, new ConfigDescription("Turns the mod on or off and opens the configuration panel.",
+                    null, new ConfigurationManagerAttributes { IsAdvanced = false, Order = 1 }));
+
+
+            }
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+Saryn's Absolutely Inoperative Nonsense
+
+This is a client mod that does nothing. It's literally a checkbox. It's an April Fool's Joke.
+
+There's no mod.
+
+If you install and run this it'll do nothing. It's a checkbox with no function. It's a joke.
+
+Happy April Fools!
